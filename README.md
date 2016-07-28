b790a83b06094c9160d4d634bcb94fcfb1d99a9b
Florian Zeller <florian.zeller@swisscom.com>
2016-07-28 10:02:26 +0200

    Initial version, templates list and detail view with git hash

---
+++ README.md
@@ -0,0 +1,18 @@
+# TowerFE
+
+## Development Environment
+
+    gem install sinatra restart
+
+For Windows, additionally
+
+    gem install wdm
+
+
+Run:
+
+restart ruby towerfe.rb
+
+git api
+
+https://git.swisscom.ch/rest/api/1.0/projects/ANSIBLE/repos/project-swisscom-portal/commits/?until=master
---
+++ towerfe.rb
@@ -0,0 +1,44 @@
+require 'sinatra'
+require 'rest-client'
+require 'json'
+
+get '/templates' do
+  json = get_job_templates(params['project'], params['playbook'])
+  @templates = json['results']
+  erb :templates
+end
+
+get '/templates/:id' do
+  json = get_job_template("#{params['id']}")
+  last_job_id = json['summary_fields']['recent_jobs'].at(0)['id']
+  @template = json
+  @gitinfo = get_git_info(last_job_id)
+  erb :template
+end
+
+def get_git_info(job_id)
+  raw = get_tower('/project_updates/' + (job_id+1).to_s + '/stdout/?format=txt')
+  search = '"after": '
+  from = raw.index(search) + search.length + 1
+  raw = raw[from..raw.length]
+  raw = raw[0..raw.index('"')-1]
+end
+
+def get_job_template(id)
+  JSON.parse(get_tower('/job_templates/' + id))
+end
+
+def get_job_templates(project=nil, playbook=nil)
+  query = '/job_templates/?page_size=50'
+  if (project) then
+    query += '&project=' + project
+  end
+  if (playbook) then
+    query += '&playbook=' + playbook
+  end
+  JSON.parse(get_tower(query))
+end
+
+def get_tower(resource)
+  RestClient.get 'https://ansible.it.bwns.ch/api/v1' + resource, {:Authorization => 'Basic dGFhemVmbDE6bG9naW4xMjM='}
+end
