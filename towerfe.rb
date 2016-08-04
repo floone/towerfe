aad2e917f45cc17008ca6254d004dd6adf025392
Florian Zeller <florian.zeller@swisscom.com>
2016-08-04 12:28:38 +0200

    Add project info, sanitize input

--- towerfe.rb
+++ towerfe.rb
@@ -4,21 +4,40 @@ require 'json'
 #require 'parallel'
 
 get '/templates/' do
-  json = get_job_templates(params['project'], params['playbook'])
-  #Parallel.each(json['results']) do |t|
-  json['results'].each do |t|
-    recent = t['summary_fields']['recent_jobs']
-    if recent.length > 0 then
-      hash = get_git_info(recent.at(0)['id'])
-      t['summary_fields']['last_job']['hash'] = hash
+  @query = ''
+  q = params['q']
+  if (q) then
+    raise "q must match the rules" unless q =~ /^[a-zA-Z0-9_ \.@]+$/
+    @query = q
+    if q.end_with? '.yml' then
+      json = get_job_templates_by_playbook(q)
+    elsif q.length > 0 then
+      json = get_job_templates_by_name(q)
     end
   end
-  @templates = json['results']
+  if (json)
+    projects = get_projects()
+    #Parallel.each(json['results']) do |t|
+    json['results'].each do |t|
+      project = projects[t['project']]
+      t['project_scm_url'] = project['scm_url'];
+      t['project_scm_branch'] = project['scm_branch'];
+      recent = t['summary_fields']['recent_jobs']
+      if recent.length > 0 then
+        hash = get_git_info(recent.at(0)['id'])
+        t['summary_fields']['last_job']['hash'] = hash
+        t['summary_fields']['last_job']['gitinfo'] = git('log -1 --pretty="format: %h %<(20,trunc)%s (%cr)" ' + hash)
+      end
+    end
+    @templates = json['results']
+  end
   erb :templates
 end
 
 get '/templates/:id' do
-  json = get_job_template("#{params['id']}")
+  id = params['id']
+  raise "id must be numeric" unless id =~ /^[0-9]+$/
+  json = get_job_template("#{id}")
   last_job_id = json['summary_fields']['recent_jobs'].at(0)['id']
   @template = json
   @gitinfo = get_git_info(last_job_id)
@@ -38,21 +57,36 @@ def get_git_info(job_id)
   end
 end
 
+def get_projects()
+  list = JSON.parse(get_tower('/projects/'))['results']
+  hash = Hash.new
+  list.each do |project|
+    hash[project['id']] = project
+  end
+  hash
+end
+
 def get_job_template(id)
   JSON.parse(get_tower('/job_templates/' + id))
 end
 
-def get_job_templates(project=nil, playbook=nil)
-  query = '/job_templates/?page_size=50'
-  if (project) then
-    query += '&project=' + project
-  end
-  if (playbook) then
-    query += '&playbook=' + playbook
-  end
+def get_job_templates_by_playbook(playbook)
+  get_job_templates('playbook=' + playbook)
+end
+
+def get_job_templates_by_name(fragment)
+  get_job_templates('name__icontains=' + fragment)
+end
+
+def get_job_templates(querystring)
+  query = '/job_templates/?page_size=50&' + querystring
   JSON.parse(get_tower(query))
 end
 
 def get_tower(resource)
   RestClient.get 'https://ansible.it.bwns.ch/api/v1' + resource, {:Authorization => 'Basic dGFhemVmbDE6bG9naW4xMjM='}
+end
+
+def git(cmd)
+  `git -C git/workingcopy #{cmd}`
 end
